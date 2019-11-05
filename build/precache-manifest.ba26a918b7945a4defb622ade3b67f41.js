7317924dd7df268627ade49deba1d8f679e759d2
scifipanda <50062729+scifipanda@users.noreply.github.com>
2019-11-05 14:01:26 -0500

    build changes

---
+++ build/precache-manifest.ba26a918b7945a4defb622ade3b67f41.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d812b79b811fda35e314ebf3adf2b256",
+    "url": "/nlss-live-check/index.html"
+  },
+  {
+    "revision": "7a806c06df89880d3f99",
+    "url": "/nlss-live-check/static/css/main.ecf32cb8.chunk.css"
+  },
+  {
+    "revision": "79dd7b090b1760d293e5",
+    "url": "/nlss-live-check/static/js/2.d8dcc322.chunk.js"
+  },
+  {
+    "revision": "7a806c06df89880d3f99",
+    "url": "/nlss-live-check/static/js/main.3a961afb.chunk.js"
+  },
+  {
+    "revision": "12b2ff66283a8513f07b",
+    "url": "/nlss-live-check/static/js/runtime-main.3a1f3ccb.js"
+  },
+  {
+    "revision": "8df6c139478949b74bb4ddc0473941cc",
+    "url": "/nlss-live-check/static/media/twitter.8df6c139.svg"
+  },
+  {
+    "revision": "96a575c41b9f0b8743beb28469ba87ed",
+    "url": "/nlss-live-check/static/media/twitterDark.96a575c4.svg"
+  },
+  {
+    "revision": "a90a07aabcb30770e05a85263a5effd4",
+    "url": "/nlss-live-check/static/media/youtube.a90a07aa.svg"
+  },
+  {
+    "revision": "7da90abbb002df291f6da82de246a8a2",
+    "url": "/nlss-live-check/static/media/youtubeDark.7da90abb.svg"
+  }
+]);
